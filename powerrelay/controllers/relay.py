74894addc32a40d7e0de879a5706340f7b56700a
Rasmus Villemoes <rasmus.villemoes@prevas.dk>
2020-08-21 12:35:49 +0200

    giant refactoring: allow gpio line names and use upstream libgpiod bindings
    This started from a desire to use a gpio line name (as set in device
    tree) rather than a (chip, line offset) specification, since we've
    seen the latter cause real problems when sometimes the pi-hat's
    gpiochip became gpiochip2 instead of gpiochip1. Unfortunately, that
    required somewhat invasive changes, since the code was built around
    interfacing with a gpiochip instance. Also, the drivers/libgpiod CDLL
    module didn't have the required wrapper for gpiod_find_line(const
    char*).
    
    So this refactors the code to use the Python binding module shipped
    with upstream libgpiod, so all we need to remember for each relay is
    an gpiod.Line instance, and updates the config file parsing to accept
    a name instead of a chip,line pair. While there, it occured to me that
    the "unique id per relay" could more easily be enforced by having
    relays be a dict of dicts, instead of a list of dicts each having an
    "id" key. The "don't specify the same gpio line twice" is
    automatically enforced since the second time we'd request it we'd get
    an error.
    
    Also remove a few things that aiohttp have deprecated (the loop
    argument and returning rather than raising a HTTPException).
    
    I've tried to preserve the REST API, so other code interfacing with
    the powerrelay service should not be affected. The config file format
    is slightly changed, but that's by design: It forces people to update
    their config, giving them an incentive to start using gpio line names.

--- powerrelay/controllers/relay.py
+++ powerrelay/controllers/relay.py
@@ -1,103 +1,89 @@
 from aiohttp import web
-from powerrelay.drivers.gpio import IN, OUT, HIGH, LOW
+import json
 
 class RelayController:
     """
         RelayAPI Controller
     """
-    def __init__(self, gpio, relaymapping):
-        self.gpio = gpio
-        self.mapping = relaymapping
-        self.relays_count = len(relaymapping)
+    def __init__(self, lines):
+        self.lines = lines
+        self.line_count = len(lines)
+        self.by_name = dict()
+        for line in lines.values():
+            name = line.name()
+            if name:
+                self.by_name[name] = line
 
-    @staticmethod
-    def relay_status(relay, value):
-        if (relay['active'] == "high" and value == 1 or
-            relay['active'] == "low" and value == 0):
-            return "1"
-        else:
-            return "0"
+    @classmethod
+    def dumps(self, x):
+        return json.dumps(x, indent=2) + "\n"
 
-    @staticmethod
-    def try_intParse(value):
-        try:
-            return int(value), True
-        except ValueError:
-            return value, False
+    @classmethod
+    def json_response(self, data):
+        return web.json_response(data, dumps=self.dumps)
+
+    def lookup_line(self, s):
+        # First try if it matches an id given in the config file
+        if s in self.lines:
+            return self.lines[s]
+        if s in self.by_name:
+            return self.by_name[s]
+        raise web.HTTPNotFound(text="Relay '%s' not found" % s)
 
     async def relays(self,request):
         """
             Return the lines resources
         """
         relays = []
-        for relay in self.mapping:
-            relay_value = self.gpio.get(relay['chip']).value(relay['line'])
-            relays.append({'id': relay['id'], 'state': RelayController.relay_status(relay,relay_value)})
+        for ident,line in self.lines.items():
+            d = {'id': ident, 'state': str(line.get_value())}
+            name = line.name()
+            if name:
+                d['name'] =  name
+            relays.append(d)
 
-        return web.json_response(relays)
+        return self.json_response(relays)
 
     async def num_relays(self,request):
         """
             Return the number of relays
         """
-        return web.json_response({"count": self.relays_count})
+        return self.json_response({"count": self.line_count})
 
     async def status(self,request):
         """
             Relay status
         """
-        try:
-            relay = int(request.match_info['relay'])
-            relay_mapped = next((x for x in self.mapping if x['id'] == relay),None)
-            if relay_mapped == None:
-                return web.HTTPError()
+        ident = request.match_info['relay']
+        line = self.lookup_line(ident)
 
-            relay_value = self.gpio.get(relay_mapped['chip']).value(relay_mapped['line'])
-            res = {"relay_id": relay,
-                   "status": RelayController.relay_status(relay_mapped,relay_value)
-            }
-            return web.json_response(res)
-        except ValueError:
-            return web.HTTPNotFound(text="Relay not found")
+        value = line.get_value()
+        res = {"relay_id": ident,
+               "status": str(value)}
+        return self.json_response(res)
 
     async def get_state(self,request):
         """
             Relay get value
         """
-        try:
-            relay = int(request.match_info['relay'])
-            relay_mapped = next((x for x in self.mapping if x['id'] == relay),None)
-            if relay_mapped == None:
-                return web.HTTPError()
+        import sys
+        ident = request.match_info['relay']
+        line = self.lookup_line(ident)
 
-            relay_value = self.gpio.get(relay_mapped['chip']).value(relay_mapped['line'])
-            return web.json_response({"state": RelayController.relay_status(relay_mapped,relay_value)})
-
-        except ValueError:
-            return web.HTTPNotFound(text="Relay not found")
+        return self.json_response({"state": str(line.get_value())})
 
     async def set_state(self,request):
         """
             Relay set value
         """
-        relay,status= RelayController.try_intParse(request.match_info['relay'])
-        if not status:
-            return web.HTTPNotFound(text="Relay not found")
+        ident = request.match_info['relay']
+        line = self.lookup_line(ident)
 
-        state_in,status = RelayController.try_intParse(request.match_info['state'])
-        if not status:
-            return web.HTTPNotFound(text="State invalid")
+        try:
+            value = int(request.match_info['state'])
+            assert(0 <= value <= 1)
+        except:
+            raise web.HTTPBadRequest(text="Invalid state, must be 0 or 1")
 
-        if state_in < 0 or state_in > 1:
-            return web.HTTPError()
-
-        relay_mapped = next((x for x in self.mapping if x['id'] == relay),None)
-        if relay_mapped == None:
-            return web.HTTPError()
-
-        if relay_mapped['active'] == 'high':
-            self.gpio.get(relay_mapped['chip']).output(relay_mapped['line'], HIGH if state_in else LOW)
-        elif relay_mapped['active'] == 'low':
-            self.gpio.get(relay_mapped['chip']).output(relay_mapped['line'], LOW if state_in else HIGH)
-
-        return web.json_response({"status": "ok"})
+        line.set_value(value)
+        return self.json_response({"status": "ok"})
